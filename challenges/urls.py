8935caaa64a4bfb4ba52b8ebbfc118af808d14d5
ankit <ankit.sanghavi87@gmail.com>
2022-05-10 23:55:38 +0530

    dynamic url

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 
 
 urlpatterns = [
-    path("january", views.index),
-    path("february", views.febIndex),
+    path("<month>", views.monthly_challenge),
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,11 +1,16 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Hello, world!")
-
-
-def febIndex(request):
-    return HttpResponse("Hello february")
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == 'january':
+        challenge_text = "Bad request"
+    elif month == 'february':
+        challenge_text = "Not found"
+    elif month == 'march':
+        challenge_text = "I'm a teapot"
+    else:
+        return HttpResponseNotFound("This month aint supported!")
+    return HttpResponse(challenge_text)
